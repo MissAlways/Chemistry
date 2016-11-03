88bdf05de8e6254e7b67d25c7bccbcb92f0153b1
Aina Kiuru <ainakiurus@gmail.com>
2016-11-03 18:02:19 +0200

    Format and remove not needed code

--- src/main/java/com/missalways/chemistry/Chemistry.java
+++ src/main/java/com/missalways/chemistry/Chemistry.java
@@ -1,12 +1,15 @@
 package com.missalways.chemistry;
 
 
+import com.missalways.chemistry.init.ModBlocks;
+import com.missalways.chemistry.init.ModCrafting;
 import com.missalways.chemistry.init.ModItems;
 import com.missalways.chemistry.proxy.CommonProxy;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -24,23 +27,29 @@ public class Chemistry {
     @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static CommonProxy proxy;
 
+    public static final CreativeTabs CREATIVE_TAB = new ChemistryTab();
+
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event){
+    public void preInit(FMLPreInitializationEvent event) {
         System.out.println("Pre Init");
 
         ModItems.init();
         ModItems.register();
+        ModBlocks.init();
+        ModBlocks.register();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event){
+    public void init(FMLInitializationEvent event) {
         System.out.println("Init");
 
         proxy.init();
+
+        ModCrafting.register();
     }
 
     @EventHandler
-    public void postInit(FMLPostInitializationEvent event){
+    public void postInit(FMLPostInitializationEvent event) {
         System.out.println("Post Init");
     }
 
